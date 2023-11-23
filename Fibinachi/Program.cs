b0f59e5f69d466670ee1f3ee4027a3fa30e7c212
15989 <15989@C406A2>
2023-11-23 10:37:13 +0200

    Add project files.

---
+++ Fibinachi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Fibinachi
+{
+    class Program
+    {
+        static void Aps()
+        {
+            double a = 1;
+            double b = 2;
+            double c = 0;
+            double max = Math.Pow(10, 15);
+
+            Stopwatch sw = Stopwatch.StartNew();
+
+            while (c <= max)
+            {
+                c = a * b;
+                b = a;
+                a = c;
+                Console.WriteLine(c);
+            }
+
+            sw.Stop();
+            Console.WriteLine("Laikas: " + sw.ElapsedMilliseconds + "ms");
+        }
+
+        static void Main(string[] args)
+        {
+            Aps();
+        }
+    }
+}
